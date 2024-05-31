63813104de2e8a10d6b28aa35507ef1b7327cef5
jhonatanmizu <natanjesuss20@gmail.com>
2024-05-31 12:47:08 -0300

    :sparkles: feat: create footer component

---
+++ src/app/common/components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="min-h-14 w-full px-4 py-2 flex bg-primary items-center justify-center">
+      <p className="font-bold">Developed with love by Jhonatanmizu</p>
+    </footer>
+  );
+};
+
+export default Footer;
