d9b847f7b4796d5d63a6410cacedfe37355448e9
jhonatanmizu <natanjesuss20@gmail.com>
2024-05-31 12:53:31 -0300

    :sparkles: feat: create Header component

---
+++ src/app/common/components/Header.tsx
@@ -0,0 +1,13 @@
+const Header = () => {
+  return (
+    <header className="min-h-14 w-full px-4 py-2 flex bg-primary items-center">
+      <div>
+        <h1 className="text-xl font-bold  md:text-sm ">
+          SNUTS.js: Sniffing Nasty Unit Test Smells in Javascript
+        </h1>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
