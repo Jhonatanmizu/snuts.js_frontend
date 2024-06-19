bb05fb1cdbccf8848061075402f58abd93023f70
jhonatanmizu <natanjesuss20@gmail.com>
2024-06-19 00:38:58 -0300

    :wrench: chore: add headers

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,19 @@ const nextConfig = {
   env: {
     BASE_URL: process.env.BASE_URL,
   },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Referrer-Policy",
+            value: "strict-origin-when-cross-origin",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
